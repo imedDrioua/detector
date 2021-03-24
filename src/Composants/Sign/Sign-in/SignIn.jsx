0fdfe4d3c2833661f6b929cdb11e299686e5cfc6
unknown <ii_drioua@esi.dz>
2021-03-24 13:40:13 +0100

    sign in and up backend

--- src/Composants/Sign/Sign-in/SignIn.jsx
+++ src/Composants/Sign/Sign-in/SignIn.jsx
@@ -23,7 +23,7 @@ function SignIn(){
         })
     }
     return(<div className={"d-flex justify-content-center"}>
-        <form className={"sign"} method={"post"} action={"http://localhost:3001/signin"}>
+        <form className={"sign"} >
             <Tilt className="Tilt  mb-3 logo" options={{ max : 55 ,scale :1.5 ,speed:10 }} style={{ height: "150px", width: "250px" }} >
                 <div className="Tilt-inner"> <img src={logo} alt={"error"}/> </div>
             </Tilt>
@@ -32,12 +32,12 @@ function SignIn(){
             <label htmlFor="floatingInput">Adresse email</label>
         </div>
             <div className="form-floating">
-                <input type="password" onChange={onMtptappe} name={"mtp"} className="form-control" id="floatingPassword" placeholder="Password"/>
+                <input type="password" onChange={onMtptappe} className="form-control" id="floatingPassword" placeholder="Password"/>
                 <label htmlFor="floatingPassword">Mot de passe</label>
             </div>
             <Link style={{textDecoration : "none"}} to={"/home"}  >
             <div className="d-grid ">
-                    <button type={"submit"} className={"btn btn-success btn-lg mt-2 shadow-lg btnMain"}>Connecter</button>
+                    <button type={"submit"} onClick={signin} className={"btn btn-success btn-lg mt-2 shadow-lg btnMain"}>Connecter</button>
                 </div>
             </Link>
             <p className={"m-hint"}>Ou </p>
