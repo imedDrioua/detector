9f8df70d28ff58f1e6ae99f24464902a466c92c3
Drimed <ii_drioua@esi.dz>
2022-08-17 22:29:42 +0100

    Profile design implementation

--- src/Composants/profile/profile.jsx
+++ src/Composants/profile/profile.jsx
@@ -1,24 +1,36 @@
 
-
+import userAvatar  from "../../images/avatar.png"
 
 import "./profile.css";
 const Profile = ({user})=>{
     return(
         <div className={"container profile-container"}>
             <div className={"header"}>
-                <div className={"heading"}>
-                    ici
-                </div>
-                <h1> My  color identifier </h1>
-                <label htmlFor={"name"}> Nom  </label> <input id={"name"} type={"text"} />
-                <label>Prenom </label>
-                <label>E-mail</label>
-                <label></label>
+            <h3>Mon Profile </h3>
 
             </div>
+            <img src={userAvatar} alt="Avatar" className="avatar" />
+        <div className={"container infos "}>
+              <form className={" form_custom"}>
+                  <label htmlFor={"nom"}> Nom :</label>
 
+                  <text id={"nom"}>Drioua</text>
+                  <br/>
+                  <label htmlFor={"prenom"}>Prenom :</label>
 
+                  <text id={"prenom"}>Imed</text>
+                  <br/>
+                  <label htmlFor={"mail"}>E-mail : </label>
+
+                  <text id={"mail"}>ii_drioua@esi.dz</text>
+                  <br/>
+                  <label htmlFor={"tlf"}>Téléphone :</label>
+
+                  <text id={"tlf"}>+213 541874128</text>
+              </form>
         </div>
+        </div>
+
     )
 }
 
