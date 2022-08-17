0b256f3a39b1aab721480b3b7a46d4fc73cb0b4e
Drimed <ii_drioua@esi.dz>
2022-08-17 17:35:48 +0100

    profile header

--- src/Composants/profile/profile.jsx
+++ src/Composants/profile/profile.jsx
@@ -6,6 +6,9 @@ const Profile = ({user})=>{
     return(
         <div className={"container profile-container"}>
             <div className={"header"}>
+                <div className={"heading"}>
+                    ici
+                </div>
                 <h1> My  color identifier </h1>
                 <label htmlFor={"name"}> Nom  </label> <input id={"name"} type={"text"} />
                 <label>Prenom </label>
